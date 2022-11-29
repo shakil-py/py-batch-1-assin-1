27f4bb997c40846734349bbafbcc835d923342a4
shakil-py <shakilhaquepy@gmail.com>
2022-11-29 23:25:19 +0600

    add

--- problem-4.py
+++ problem-4.py
@@ -1,9 +1,9 @@
 list_1=[5,15,20,12,20,13,20,14,20]
-
+print("frist list : ",list_1)
 for i in range(len(list_1)):
     # if list_1[i]==20:
     #     list_1=list_1.remove(20)
     #     print(list_1)
     if 20 in list_1:
         list_1.remove(20)
-print(list_1)
+print("After loop list : ",list_1)
