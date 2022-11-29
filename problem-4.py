ef5fb294d85adb93603303627c6e3374dfdaf291
shakil-py <shakilhaquepy@gmail.com>
2022-11-29 23:21:27 +0600

    add

--- problem-4.py
+++ problem-4.py
@@ -1,12 +1,9 @@
 list_1=[5,15,20,12,20,13,20,14,20]
-# list_1.remove(7)
 
-# list_2=list_1.sort()
-# print(list_2)
-# print(list_1)
 for i in range(len(list_1)):
-    if list_1[i]==20:
-        list_1[i]=list_1.remove(7)
+    # if list_1[i]==20:
+    #     list_1=list_1.remove(20)
+    #     print(list_1)
+    if 20 in list_1:
+        list_1.remove(20)
         print(list_1)
-# list_2=set(list_1)
-# print(list_2)
