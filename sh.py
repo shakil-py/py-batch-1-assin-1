759cf84802863df63727f7d31171d9f53ecba0e0
shakil-py <shakilhaquepy@gmail.com>
2022-11-29 14:57:30 +0600

    a

---
+++ sh.py
@@ -0,0 +1,11 @@
+
+my_input_1 = input("Please input list-1 number useing ',' :")
+my_input_2 = input("Please input list-2 number useing ',' :")
+
+list_1 = my_input_1.split(",")
+list_2 = my_input_2.split(",")
+
+for i in range(len(list_1)):
+    list = int(list_2[i])+int(list_1[i])
+    print(list)
+
