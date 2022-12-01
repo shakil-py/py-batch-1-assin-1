093c3440deb43858cf37053728cfc7918d2c5c80
shakil-py <shakilhaquepy@gmail.com>
2022-12-01 16:18:40 +0600

    add

--- problem-1.py
+++ problem-1.py
@@ -3,13 +3,21 @@
 ###################################################################
 
 
+###################################################################
+## Take input 2 int list and Concatinate two list index wise ##
+###################################################################
+
+
 my_input_1 = input("Please input list-1 number useing ',' :")
 my_input_2 = input("Please input list-2 number useing ',' :")
 
 list_1 = my_input_1.split(",")
 list_2 = my_input_2.split(",")
+list_3 = []
 
 for i in range(len(list_1)):
     list = int(list_2[i])+int(list_1[i])
-    print(list,end=",")
+    list_3.append(list)
 
+
+print(list_3)
